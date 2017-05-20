3dd0c6216233869833cfe80c2952344c533389c6
Edward John Steere <edward.steere@gmail.com>
2017-05-20 15:39:11 +0200

    Adds stubs and basic evaluation support for Emacs Lisp
    So far I've only implemented evaluation with read then eval.
     - incremental-eval-emacs-lisp.el:incremenatl-eval-setup-emacs-lisp added
     - incremental-eval-emacs-lisp.el:incremental-eval-emacs-lisp added
     - incremental-eval-emacs-lisp.el:incremental-eval-next-form-is-a-macro-emacs-lisp-p
       added
     - incremental-eval-emacs-lisp.el:incremental-eval-next-form-is-a-branch-emacs-lisp-p
       added
     - incremental-eval-emacs-lisp.el:incremental-eval-done-evaluating-emacs-lisp-p
       added

---
+++ incremental-eval-emacs-lisp.el
@@ -0,0 +1,40 @@
+;;; incremental-eval-emacs-lisp --- setup hooks for incremental eval in Emacs Lisp mode -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(defun incremenatl-eval-setup-emacs-lisp ()
+  "Setup Emacs Lisp mode for incremental eval mode."
+  (interactive)
+  (setq-local incremental-eval-engine                  #'incremental-eval-emacs-lisp)
+  (setq-local incremental-eval-next-form-is-a-macro-p  #'incremental-eval-next-form-is-a-macro-emacs-lisp-p)
+  (setq-local incremental-eval-next-form-is-a-branch-p #'incremental-eval-next-form-is-a-branch-emacs-lisp-p)
+  (setq-local incremental-eval-done-evaluating-p       #'incremental-eval-done-evaluating-emacs-lisp-p))
+
+(defun incremental-eval-emacs-lisp (string-expression)
+  "Evauate STRING-EXPRESSION using read and eval."
+  (eval (read string-expression)))
+
+(defun incremental-eval-next-form-is-a-macro-emacs-lisp-p ()
+  "Produce t if the next form is a macro in Emacs Lisp.
+
+TODO!"
+  nil)
+
+(defun incremental-eval-next-form-is-a-branch-emacs-lisp-p ()
+  "Produce t if the next form is a conditional in Emacs Lisp.
+
+TODO!"
+  nil)
+
+(defun incremental-eval-done-evaluating-emacs-lisp-p ()
+  "Produce t if we're don evaluating in Emcas Lisp.
+
+TODO!"
+  nil)
+
+(add-hook 'emacs-lisp-mode-hook #'incremenatl-eval-setup-emacs-lisp)
+
+(provide 'incremental-eval-emacs-lisp)
+;;; incremental-eval-emacs-lisp ends here
