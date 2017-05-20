7e395534cd9a8efa367013a340b1df12a2da6ee2
Edward John Steere <edward.steere@gmail.com>
2017-05-20 09:50:10 +0200

    Adds initial source file
     - incremental-eval.el added skeleton of lisp file

---
+++ incremental-eval.el
@@ -0,0 +1,8 @@
+;;; incremental-eval --- incrementally, and interactively evaluate lisp forms -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(provide 'incremental-eval)
+;;; incremental-eval ends here
